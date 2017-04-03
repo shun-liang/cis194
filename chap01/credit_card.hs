cfa19ebe2e53affe651fba68bda9b88f3cc957b4
Shun Liang <shun.liang.public@gmail.com>
2017-04-03 21:41:57 +0100

    Chap 1 exercise 3

--- chap01/credit_card.hs
+++ chap01/credit_card.hs
@@ -14,3 +14,13 @@ doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (first : []) = [first]
 doubleEveryOther (first : second : xs) = first : (2 * second) : doubleEveryOther xs
+
+sumDigitsOfANum :: Integer -> Integer
+sumDigitsOfANum 0 = 0
+sumDigitsOfANum n
+    | n < 0 = 0
+    | otherwise = (sumDigitsOfANum (n `div` 10)) + (n `mod` 10)
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x : xs) = sumDigitsOfANum x + sumDigits xs
