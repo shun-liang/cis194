bd431940ac33af32ded040cb687613a4b7a9f5a2
Shun Liang <shun.liang.public@gmail.com>
2017-04-09 17:30:35 +0100

    Chap 2 exercise 1

---
+++ chap02/LogAnalysis.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+parseMessageWords :: [String] -> LogMessage
+parseMessageWords ("I" : timeStr : restWords) = LogMessage Info (read timeStr :: Int) (unwords restWords)
+parseMessageWords ("W" : timeStr : restWords) = LogMessage Warning (read timeStr :: Int) (unwords restWords)
+parseMessageWords ("E" : errorStr : timeStr : restWords) = 
+    LogMessage (Error (read errorStr :: Int)) (read timeStr :: Int) (unwords restWords)
+parseMessageWords strList = Unknown (unwords strList)
+
+parseMessage :: String -> LogMessage
+parseMessage string = parseMessageWords (words string)
